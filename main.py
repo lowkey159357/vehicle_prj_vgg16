00af9b0c59d032880fe214643f07d2717f3dc2d5
lowkey159357 <40413770+lowkey159357@users.noreply.github.com>
2018-08-09 18:53:17 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-# -*- coding: utf-8 -*-
+# coding: utf-8 
 
 from __future__ import absolute_import
 from __future__ import division
@@ -7,9 +7,8 @@ from __future__ import print_function
 
 import argparse
 import os
-
 import sys
-sys.setdefaultencoding('utf-8')
+
 
 #dataset_dir = "C:\\jupyter_work\lastweek\\vgg_16_adverse_small_dataset\\image_dataset"
 #logs_train_dir = "C:\\jupyter_work\lastweek\\vgg_16_adverse_small_dataset\\out_log"
